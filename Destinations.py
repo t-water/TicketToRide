9c7dfa9fc96017707b2ae47cd30ab0fb120be1c9
Tyler Waterman <watermant@hhmi.org>
2022-02-04 20:17:09 -0500

    Created destinations enum

---
+++ Destinations.py
@@ -0,0 +1,21 @@
+from enum import Enum, auto
+
+class Destinations(Enum):
+    PICCADILLY_CIRCUS = auto()
+    HYDE_PARK = auto()
+    TRAFALGAR_SQUARE = auto()
+    THE_GLOBE_THEATRE = auto()
+    BRITISH_MUSEUM = auto()
+    BIG_BEN = auto()
+    BAKER_STREET = auto()
+    BUCKINGHAM_PALACE = auto()
+    KINGS_CROSS = auto()
+    HYDE_PARK
+    COVENT_GARDEN = auto()
+    REGENTS_PARK = auto()
+    WATERLOO = auto()
+    BRICK_LANE = auto()
+    ST_PAULS = auto()
+    THE_CHARTERHOUSE = auto()
+    ELEPHANT_AND_CASTLE = auto()
+    TOWER_OF_LONDON = auto()
