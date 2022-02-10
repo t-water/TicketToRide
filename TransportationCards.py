a9005b58fed3e96224a8d6f0acf9e65e1eb03346
Tyler Waterman <watermant@hhmi.org>
2022-02-09 19:23:27 -0500

    Re-combined transportation card classes

--- TransportationCards.py
+++ TransportationCards.py
@@ -1,19 +1,51 @@
-from collections import deque
-
-from DrawPile import DrawPile    
-from Flop import Flop
+from collections import Counter, deque
+import random
+import csv
 
 class TransportationCardDeck:
     def __init__(self):
-        self.__cards = DrawPile()
+        self.__cards = deque()
         self.__discard_pile = deque()
-        self.__flop = Flop()
+        self.__flop = []
+        self.__wild = 'wild'
+
+        with open('TransportationCards.csv', newline='') as csv_file:
+            card_reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
+
+            for row in card_reader:
+                entry = row[0].split(',')
+
+                self.__cards.extend([entry[0]] * int(entry[1]))
+            
+        random.shuffle(self.__cards)
+        
+        self.flop()
+
+    def draw_card(self):
+        return self.__cards.popleft()
+    
+    def draw_cards(self, numCards):
+        return [self.draw_card() for _ in range(numCards)]
     
     def initial_deal(self):
-        return self.__cards.initial_deal()
+        return self.draw_cards(2)
+
+    def __illegal_flop(self):
+        c = Counter(self.__flop)
+        
+        return c[self.__wild] >= 3
+
+    def flop(self):
+        while len(self.__flop) == 0 or self.__illegal_flop():
+            self.__discard_pile.extend(self.__flop)
+            self.__flop.clear()
+            self.__flop.extend(self.draw_cards(5))
     
     def can_take_turn(self):
         return True
     
     def take_turn(self):
-        return self.__flop.take_turn()
+        return []
+    
+    def print_flop(self):
+        print(self.__flop)
